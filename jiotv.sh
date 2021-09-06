5ee822613083a29a765406ea92e00ab66f510069
soal001 <jspader90@gmail.com>
2021-09-06 15:50:12 +0530

    Added Linux Script

---
+++ jiotv.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt -y install apache2 php libapache2-mod-php unzip wget git
+sudo apt -y install php php-cli php-fpm php-json php-common php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath
+git clone https://github.com/soal001/jiotv
+sudo chmod -R 777 /var/www/html
+sudo rm /var/www/html/*
+sudo cp -r jiotv/. /var/www/html/
+sudo rm -rf jiotv/
