2a6a5923565efe73bcda65c93f766f2d55910840
soal001 <81707847+soal001@users.noreply.github.com>
2021-09-12 12:05:21 +0530

    Added Android Auto Install Script

--- android.sh
+++ android.sh
@@ -1,11 +1,10 @@
 apt update
 apt upgrade
-apt install php-apache git -y
-cd /data/data/com.termux/files/usr/etc/apache2/
-rm httpd.conf
+apt install -y php-apache git
+rm /data/data/com.termux/files/usr/etc/apache2/httpd.conf
 wget https://github.com/soal001/jiotv/blob/main/httpd.conf
-cd /data/data/com.termux/files/usr/share/apache2/default-site/htdocs
+mv httpd.conf /data/data/com.termux/files/usr/etc/apache2/
 git clone https://github.com/soal001/jiotv
-cp -r jiotv/* ./ && rm -rf jiotv/
-rm http.conf android.sh jiotv.sh
+cp -r jiotv/* /data/data/com.termux/files/usr/share/apache2/default-site/htdocs && rm -rf jiotv/
+rm /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/http.conf /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/android.sh /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/jiotv.sh
 apachectl start
