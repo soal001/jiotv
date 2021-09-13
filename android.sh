9fcfc63669f845ef2a8498363ba4e5a9f07ba114
soal001 <81707847+soal001@users.noreply.github.com>
2021-09-13 12:50:39 +0530

    Added Output of IP Address

--- android.sh
+++ android.sh
@@ -8,7 +8,7 @@ git clone https://github.com/soal001/jiotv
 cp -r jiotv/* /data/data/com.termux/files/usr/share/apache2/default-site/htdocs && rm -rf jiotv/
 rm /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/http.conf /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/android.sh /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/jiotv.sh
 ip route get 1.2.3.4 | awk '{print $7}' > ipaddr.txt
-echo ':8080'
+echo ':8080' >> ipaddr.txt
 apachectl start
 clear
 tr --delete '\n' < ipaddr.txt
