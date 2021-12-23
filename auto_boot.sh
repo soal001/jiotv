9e9c03932b5c939f8fcaba5c8f74b99526fced45
soal001 <81707847+soal001@users.noreply.github.com>
2021-12-23 18:48:01 +0530

    Added Auto-Start Script

---
+++ auto_boot.sh
@@ -0,0 +1,6 @@
+apachectl start
+termux-wake-lock
+echo 'JIO TV' | figlet
+echo 'PHP \n SERVER' | figlet
+echo '\033[42m Server Installed Successfully.\n Go to the below Address \n to login and continue setup \033[0m'
+echo '\033[32m '`ip route get 1.2.3.4 | awk '{print $9}'`':8080\n\033[0m'
