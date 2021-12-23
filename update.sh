bf62e0bdc54e2fe516a0609b1844eee5ab1ae5d3
soal001 <81707847+soal001@users.noreply.github.com>
2021-12-23 18:47:14 +0530

    Added Auto-Start on boot

--- update.sh
+++ update.sh
@@ -3,8 +3,11 @@ rm -rf /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/
 mkdir /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/
 cp -r jiotv/* /data/data/com.termux/files/usr/share/apache2/default-site/htdocs && rm -rf jiotv/
 rm /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/httpd.conf /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/android.sh /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/jiotv.sh
-ip route get 1.2.3.4 | awk '{print $9}' > ipaddr.txt
-echo ':8080' >> ipaddr.txt
+mkdir .termux/boot
+mv /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/auto_boot.sh ./.termux/boot/
 apachectl start
-clear
-tr --delete '\n' < ipaddr.txt
+termux-wake-lock
+echo 'JIO TV' | figlet
+echo 'PHP \n SERVER' | figlet
+echo '\033[42m Server Installed Successfully.\n Go to the below Address \n to login and continue setup \033[0m'
+echo '\033[32m '`ip route get 1.2.3.4 | awk '{print $9}'`':8080\n\033[0m'
